17a4607e29dd8b60d00d687996c8377b4f5cc44f
Abdul-Afeez <zeldif14@gmail.com>
2025-01-21 12:43:34 +0100

    dfdfdf

--- app.py
+++ app.py
@@ -5,3 +5,20 @@ import sqlite3
 import requests
 from datetime import datetime
 import os
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Welcome to github !"
+
+@app.route('/helpers')
+def help():
+    return "This is the Helpers page."
+
+@app.route('/about')
+def about():
+    return "This is the About page."
+
+if __name__ == '__main__':
+    app.run(debug=True)
