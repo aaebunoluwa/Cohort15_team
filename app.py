4c7f608a991c254356fcb28f128a696d6e2cc2af
unknown <alukoj62@gmail.com>
2025-01-21 12:40:38 -0800

    addingin to app.py

--- app.py
+++ app.py
@@ -1,17 +1,7 @@
 from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "Welcome to github !"
-
-@app.route('/helpers')
-def help():
-    return "This is the Helpers page."
-
-@app.route('/about')
-def about():
-    return "This is the About page."
-
-if __name__ == '__main__':
-    app.run(debug=True)
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+from werkzeug.security import generate_password_hash, check_password_hash
+import sqlite3
+import requests
+from datetime import datetime
+import os
